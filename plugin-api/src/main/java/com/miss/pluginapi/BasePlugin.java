73b20cdbe28b70ff8fa256dea06027d5d4de7c2b
xuanmiss <xuanmine@gmail.com>
2024-09-02 21:22:05 +0800

    init project

---
+++ plugin-api/src/main/java/com/miss/pluginapi/BasePlugin.java
@@ -0,0 +1,13 @@
+package com.miss.pluginapi;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.pf4j.Plugin;
+
+@Getter
+@Slf4j
+public class BasePlugin extends Plugin {
+
+
+    protecte
+}
---
+++ plugin-api/src/main/java/com/miss/pluginapi/PluginContext.java
@@ -0,0 +1,21 @@
+package com.miss.pluginapi;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.pf4j.RuntimeMode;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class PluginContext {
+    
+    private final String name;
+
+    private final String configMapName;
+
+    private final String version;
+
+    private final RuntimeMode runtimeMode;
+
+}
